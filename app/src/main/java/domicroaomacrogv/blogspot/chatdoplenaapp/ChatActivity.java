f33bf426868c44c62fa734c516034f19afae76cd
gabriel88766 <gabriel.souzasilva125@gmail.com>
2020-06-18 03:30:58 -0300

    chat funcional com WebSockets e novas dependências no gradle

--- app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/ChatActivity.java
+++ app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/ChatActivity.java
@@ -2,16 +2,18 @@ package domicroaomacrogv.blogspot.chatdoplenaapp;
 
 import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,55 +21,62 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import io.reactivex.CompletableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
+
 import static domicroaomacrogv.blogspot.chatdoplenaapp.MainActivity.URL_ADD;
 
 public class ChatActivity extends AppCompatActivity {
 
     ListView list;
     ArrayAdapter<String> adapter;
-    Handler handler = new Handler();
-    Runnable runnable;
-    String myUsername;
+
     ArrayList<String> messagesNow;
     ArrayList<String> sender;
-    int qnt;
-    int oldQnt;
+    private StompClient mStompClient;
+    int userId;
+    int roomId;
+    String username;
+    String roomName;
+    private CompositeDisposable compositeDisposable;
+    String newMessageReceived;
+    String newMessageSend;
+    EditText editText;
+    JSONObject newJSONMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_activity);
+        mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws"+URL_ADD.substring(4)+"/mywebsockets/websocket");
+        getChatInfo();
         TextView room = findViewById(R.id.textView6);
         room.setText(SingletonUsuario.getInstance().getSelectedRoom());
         messagesNow = new ArrayList<>();
         sender = new ArrayList<>();
         makeMessagesListView();
-        myUsername = SingletonUsuario.getInstance().getUsuario();
-    }
+        getMessages();
 
-    // COMEÇO: o código a seguir funciona atualizando a lista de mensagens a cada 2 segundos
-    @Override
-    protected void onResume() {
-        super.onResume();
-        handler.postDelayed( runnable = () -> {
-            updateMessages();
-            handler.postDelayed(runnable, 2000);
-        }, 2000);
-    }
+        resetSubscriptions();
 
-    @Override
-    protected void onPause() {
-        handler.removeCallbacks(runnable);
-        super.onPause();
+        editText = findViewById(R.id.edit_chat);
     }
-//fim: o código acima funciona atualizando a lista de mensagens a cada 2 segundos
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mStompClient.disconnect();
+        if (compositeDisposable != null) compositeDisposable.dispose();
+    }
 
     public void back(View view){
          finish();
     }
-    public void send(View view){
-            //Enviar mensagem.
-    }
+
     void makeMessagesListView(){
 
         ArrayList<String> createLv = new ArrayList<>();
@@ -86,18 +95,18 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    void updateListView(){
-
-        if(verifyQuantity()) {
-            oldQnt=qnt;
-            adapter.clear();
-            adapter.addAll(messagesNow);
-            adapter.notifyDataSetChanged();
-        }
+    void updateListView() {
+        adapter.add(newMessageReceived);
+        adapter.notifyDataSetChanged();
+    }
+    void createListView(){
+        adapter.clear();
+        adapter.addAll(messagesNow);
+        adapter.notifyDataSetChanged();
     }
 
 
-    void updateMessages(){
+    void getMessages() {
         JsonArrayRequest request = new JsonArrayRequest
                 (Request.Method.GET, URL_ADD+"/messages",null, response -> {
                     try {
@@ -107,14 +116,13 @@ public class ChatActivity extends AppCompatActivity {
                             for (int i = 0; i < response.length(); i++) {
                                 JSONObject currentRoom = response.getJSONObject(i);
                                 messagesNow.add(currentRoom.getString("content"));
-                                sender.add(currentRoom.getString("username"));
+                                sender.add(currentRoom.getString("username")); //modificar para se adequar.
                             }
-                            qnt=response.length();
 
                         }else{
                             messagesNow = new ArrayList<>(Collections.singletonList("Nenhuma mensagem encontrada"));
                         }
-                        updateListView();
+                        createListView();
 
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -124,9 +132,99 @@ public class ChatActivity extends AppCompatActivity {
                 });
         SingletonRequestQueue.getInstance(this).addToRequestQueue(request);
     }
-
-    boolean verifyQuantity(){ //retorna verdadeiro se a quantidade de mensagens aumentar
-        return qnt!=oldQnt;
+    void getChatInfo(){
+        JsonObjectRequest request =
+                new JsonObjectRequest(Request.Method.GET,
+                                      URL_ADD+"/chatInfo",null,
+                                      response-> {
+                    try{
+                        userId = response.getInt("userId");
+                        roomId = response.getInt("roomId");
+                        username = response.getString("username");
+                        roomName = response.getString("roomName");
+                        subscribe();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                                      },error -> { /*Nothing to do */});
+        SingletonRequestQueue.getInstance(this).addToRequestQueue(request);
     }
 
+    public void sendMessage(View view) {
+        if (!mStompClient.isConnected()) return;
+        newMessageSend = editText.getText().toString();
+        editText.setText("");
+        JSONObject temp  = new JSONObject();
+
+        try {
+            temp.put("userId",userId);
+            temp.put("roomId",roomId);
+            temp.put("username",username);
+            temp.put("content",newMessageSend);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        compositeDisposable.add(mStompClient.send("/app/message",String.valueOf(temp))
+                                        .compose(applySchedulers())
+                                        .subscribe(() -> Log.d("Chat Activity","STOMP echo send successfully"),
+                                                   throwable -> Log.e("Chat Activity","Error send STOMP ",throwable)));
+    }
+
+    private void subscribe() {
+        mStompClient.withClientHeartbeat(1000).withServerHeartbeat(1000);
+        resetSubscriptions();
+        Disposable dispLifecycle = mStompClient.lifecycle()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(lifecycleEvent -> {
+                    switch (lifecycleEvent.getType()) {
+                        case OPENED:
+                            break;
+                        case ERROR:
+                            Log.e("ChatActivity","Stomp connection error",lifecycleEvent.getException());
+                            break;
+                        case CLOSED:
+                            resetSubscriptions();
+                            break;
+                        case FAILED_SERVER_HEARTBEAT:
+                            Log.e("ChatActivity","Stomp hearthbeat fail",lifecycleEvent.getException());
+                            break;
+                    }
+                });
+
+        compositeDisposable.add(dispLifecycle);
+
+        Disposable dispTopic = mStompClient.topic("/topic/"+roomId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(topicMessage -> {
+                    newJSONMessage = new JSONObject(topicMessage.getPayload());
+                    try{
+                        newMessageReceived = newJSONMessage.getString("content");
+                        // linha onde podemos saber o remetente newJSONMessage.getInt("username")
+                        updateListView();
+                    }catch(JSONException e){
+                        e.printStackTrace();
+                    }
+                },throwable -> Log.d("fail","Error on subscribe topic",throwable));
+
+        compositeDisposable.add(dispTopic);
+
+        mStompClient.connect(null);
+    }
+
+
+    private void resetSubscriptions() {
+        if (compositeDisposable != null) {
+            compositeDisposable.dispose();
+        }
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    protected CompletableTransformer applySchedulers() {
+        return upstream -> upstream
+                .unsubscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
 }
