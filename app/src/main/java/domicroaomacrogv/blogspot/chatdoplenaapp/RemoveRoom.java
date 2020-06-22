898d5a07648182f41294f625e11bd72b9925c01a
Bernardo Sobral Werneck <bernardo.s.werneck@gmail.com>
2020-06-21 21:42:18 -0300

    Conexão com a API

--- app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/RemoveRoom.java
+++ app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/RemoveRoom.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static domicroaomacrogv.blogspot.chatdoplenaapp.MainActivity.URL_ADD;
 
 public class RemoveRoom extends AppCompatActivity {
 
@@ -15,16 +21,46 @@ public class RemoveRoom extends AppCompatActivity {
     }
 
     public void onClickRemoveRoom (View view) {
-        if ( sucessRemovingRoom() ) {
+        String url = URL_ADD+"/removeRoom";
+        JSONObject body = new JSONObject();
+        try {
+            body.put("roomName", ((EditText)findViewById(R.id.editText)).getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                response -> {
+                    if(response.equals("true")){
+                        result(true);
+                    }else{
+                        result(false);
+                    }
+                },
+                error -> result(false)) {
+            @Override
+            public byte[] getBody() {
+                return body.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+        SingletonRequestQueue.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    public void result(boolean success)
+    {
+        if(success)
+        {
             finish();
             this.startActivity(new Intent(this, RoomManagement.class));
             Toast.makeText(this, "Sala removida com sucesso", Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        else
+        {
             Toast.makeText(this, "Não foi possível remover a sala", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    public boolean sucessRemovingRoom() {
-        return true;
     }
 }
--- app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/RemoveUserFromRoom.java
+++ app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/RemoveUserFromRoom.java
@@ -1,8 +1,16 @@
 package domicroaomacrogv.blogspot.chatdoplenaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static domicroaomacrogv.blogspot.chatdoplenaapp.MainActivity.URL_ADD;
 
 public class RemoveUserFromRoom extends AppCompatActivity {
 
@@ -13,6 +21,47 @@ public class RemoveUserFromRoom extends AppCompatActivity {
     }
 
     public void onClickRemove(View view){
+        String url = URL_ADD+"/removeUserFromRoom";
+        JSONObject body = new JSONObject();
+        try {
+            body.put("username", ((EditText)findViewById(R.id.editText)).getText().toString());
+            body.put("roomName", ((EditText)findViewById(R.id.editText2)).getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                response -> {
+                    if(response.equals("true")){
+                        result(true);
+                    }else{
+                        result(false);
+                    }
+                },
+                error -> result(false)) {
+            @Override
+            public byte[] getBody() {
+                return body.toString().getBytes();
+            }
 
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+        SingletonRequestQueue.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    public void result(boolean success)
+    {
+        if(success)
+        {
+            finish();
+            this.startActivity(new Intent(this, RoomManagement.class));
+            Toast.makeText(this, "Usuário removido com sucesso", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Não foi possível remover usuário da sala", Toast.LENGTH_SHORT).show();
+        }
     }
 }
