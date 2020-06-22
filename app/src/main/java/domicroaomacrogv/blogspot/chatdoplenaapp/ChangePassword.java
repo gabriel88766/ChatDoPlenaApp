e415739d2a75af4f2a6ef5c225eda02860c576a8
Bernardo Sobral Werneck <bernardo.s.werneck@gmail.com>
2020-06-22 11:22:52 -0300

    Conexão com a API

--- app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/ChangePassword.java
+++ app/src/main/java/domicroaomacrogv/blogspot/chatdoplenaapp/ChangePassword.java
@@ -1,8 +1,16 @@
 package domicroaomacrogv.blogspot.chatdoplenaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static domicroaomacrogv.blogspot.chatdoplenaapp.MainActivity.URL_ADD;
 
 public class ChangePassword extends AppCompatActivity {
 
@@ -13,6 +21,47 @@ public class ChangePassword extends AppCompatActivity {
     }
 
     public void onClickChangePassword(View view) {
+        String url = URL_ADD+"/updateUserPassword";
+        JSONObject body = new JSONObject();
+        try {
+            body.put("username", ((EditText)findViewById(R.id.editText)).getText().toString());
+            body.put("password", ((EditText)findViewById(R.id.editText2)).getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                response -> {
+                    if(response.equals("true")){
+                        result(true);
+                    }else{
+                        result(false);
+                    }
+                },
+                error -> result(false)) {
+            @Override
+            public byte[] getBody() {
+                return body.toString().getBytes();
+            }
 
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+        SingletonRequestQueue.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    public void result(boolean success)
+    {
+        if(success)
+        {
+            finish();
+            this.startActivity(new Intent(this, UserManagement.class));
+            Toast.makeText(this, "Usuário atualizado com sucesso", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Não foi possível atualizar o usuário", Toast.LENGTH_SHORT).show();
+        }
     }
 }
